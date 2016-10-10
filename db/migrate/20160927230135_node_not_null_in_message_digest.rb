8adce063040d0fd27c4d770d733647fb247769e4
Bryan Hockey <bhock@umich.edu>
2016-10-10 14:29:35 -0500

    use change_column_null instead of change_column

--- db/migrate/20160927230135_node_not_null_in_message_digest.rb
+++ db/migrate/20160927230135_node_not_null_in_message_digest.rb
@@ -10,7 +10,7 @@ class NodeNotNullInMessageDigest < ActiveRecord::Migration
     MessageDigest.where(node_id: nil).all.each do |md|
       md.update!(node_id: md.bag.admin_node_id)
     end
-    change_column :message_digests, :node_id, :integer, null: false
+    change_column_null :message_digests, :node_id, false
   end
 
 end
